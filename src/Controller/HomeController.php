4dc8a1cd0fbdd04ddfdd0f0066b4b3e87d7927f6
Thibault Berthelin <thibault.berthelin@outlook.fr>
2025-02-02 18:17:57 +0100

    home page dynamic

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CarRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,9 +10,15 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/', name: 'home_')]
 final class HomeController extends AbstractController
 {
+    public function __construct(private CarRepository $carRepository) {}
+
     #[Route('/', name: 'index')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', []);
+        $cars = $this->carRepository->findAll();
+
+        return $this->render('home/index.html.twig', [
+            "cars" => $cars
+        ]);
     }
 }
